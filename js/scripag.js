0e8ba193d74c69526d2d98afa628a3378ebd616e
albert925 <albertarias925@gmail.com>
2015-09-10 17:58:53 -0500

    menfx

--- js/scripag.js
+++ js/scripag.js
@@ -1,6 +1,7 @@
 $(document).on("ready",inicio_pagina);
 var contador=1;
 function inicio_pagina () {
+	$(window).scroll(movermenu);
 	$("#mn_mv").on("click",abrir_menu);
 }
 function abrir_menu () {
@@ -11,5 +12,15 @@ function abrir_menu () {
 	else{
 		$("#mnP").animate({left:"-100%"}, 500);
 		contador=1;
+	}
+}
+function movermenu () {
+	var altoin=$("#inic").height();
+	console.log(altoin);
+	if ($(window).scrollTop()>altoin) {
+		$("header").css({position:"fixed"});
+	}
+	else{
+		$("header").css({position:"relative"});
 	}
 }
